08ae819b942b34ed0294dfdaf4c7bf3aa21cf39f
rnd-pai <ksorn@kku.ac.th>
2017-02-02 16:17:31 +0700

    add theme

--- config/web.php
+++ config/web.php
@@ -7,6 +7,13 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'view' => [
+          'theme' => [
+            'pathMap' => [
+              '@app/views' => '@app/themes/gentelella/views',
+            ],
+          ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'GSAUsgkFmNuPnPG93JxI2X2NeQ9ngHbm',
